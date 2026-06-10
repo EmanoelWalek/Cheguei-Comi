b8566f3aee3a06e1a4d7b6f84a0cf5bcb41da19c
carlao767 <carliin2008@hotmail.com>
2026-06-10 17:19:09 -0300

    Add files via upload

--- cheguei-comi/javascript/script.js
+++ cheguei-comi/javascript/script.js
@@ -1,7 +1,3 @@
-document.getElementById("btnCardapio").addEventListener("click", function() {
-    window.location.href = "cardapio.html";
-});
-
 document.getElementById("btnCantinas").addEventListener("click", function() {
     window.location.href = "cantinas.html";
 });
